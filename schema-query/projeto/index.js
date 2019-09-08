5c0585aa4319faabd04193a92dad477831b59e28
Iago Santos <iagofalcom@hotmail.com>
2019-09-08 16:27:58 -0300

    Corrigindo Erro e criando typeDef e  resolvers

--- schema-query/projeto/index.js
+++ schema-query/projeto/index.js
@@ -1,19 +1,32 @@
 //import do Appolo e gql(gql é uma tagged de template é uma função que faz tratamento de string)
-const {AppoloServer, gql} = require('apollo-server')
+const {ApolloServer, gql} = require('apollo-server')
 
 //Definições de Tipos
-const typeDefs = gql``
+const typeDefs = gql`
+    #Ponto de entrada da API
+    type Query{
+        ola:String
+    }
 
-//
-const resolvers={}
+`
+//Resolve os dados que foram solicitados pelo typeDefs
+const resolvers={
+    Query:{
+        ola(){
+            return 'Bom Dia'
+        }
+    }
+
+
+}
 
 //Sempre usar typeDef e resolvers como nome das consts, padrão de desenvolvimento
-const server = new AppoloServer({
+const server = new ApolloServer({
     typeDefs,
     resolvers
 })
 
 //Start do servidor, da para personalizar a porta dentro do parametro do listen()
-server.listen().then(({url})=>{
+server.listen().then(({ url })=>{
     console.log(`Executando em ${url}`)
 })
