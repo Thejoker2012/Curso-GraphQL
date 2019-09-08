1c2f5fc84877a13787d92beb24b59b8912b4ea3f
Iago Santos <iagofalcom@hotmail.com>
2019-09-08 20:30:10 -0300

    Criando Enum no GraphQl

--- schema-query/projeto/data/db.js
+++ schema-query/projeto/data/db.js
@@ -8,20 +8,23 @@ const usuarios = [{
     nome:'João Silva',
     email:'jsilva@gmail.com',
     idade:29,
-    perfil_id: 1
+    perfil_id: 1,
+    status:'ATIVO'
 },{
     id:2,
     nome:'Maria Silva',
     email:'Msilva@gmail.com',
     idade:28,
-    perfil_id: 2
+    perfil_id: 2,
+    status:'INATIVO'
 
 },{
     id:3,
     nome:'Marcos Silva',
     email:'Masilva@gmail.com',
     idade:22,
-    perfil_id: 1
+    perfil_id: 1,
+    status:'BLOQUEADO'
 },]
 
 module.exports = {usuarios, perfis}
